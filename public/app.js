51e9cf395b9e83734aa2bdf8ca95676c664ead9e
Ben Krig <benkrig@Benjamins-MacBook-Pro.local>
2017-04-03 15:39:42 -0700

    blog

--- public/app.js
+++ public/app.js
@@ -54,6 +54,7 @@ controllers.global_controller = function($scope, $location)
 		{
 			image: 'blogs/ben.png',
 			title: "OpenCV: Facial Recognition using Haar Feature-based Cascade Classifiers",
+			subtitle: "Viola and Jones Method",
 			date: "4/2/17"
 		}
 		
